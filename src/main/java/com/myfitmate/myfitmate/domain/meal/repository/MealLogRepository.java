29a5743fdc93b44a5369cdf88010c533bdfd268f
psh <todo220@naver.com>
2025-06-16 22:54:41 +0900

    Feat: MealLogRepository.java - 식단 변경 로그 저장소

---
+++ src/main/java/com/myfitmate/myfitmate/domain/meal/repository/MealLogRepository.java
@@ -0,0 +1,10 @@
+package com.myfitmate.myfitmate.domain.meal.repository;
+
+import com.myfitmate.myfitmate.domain.meal.entity.MealLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MealLogRepository extends JpaRepository<MealLog, Long> {
+    List<MealLog> findByMealIdOrderByActionTimeDesc(Long mealId);
+}
