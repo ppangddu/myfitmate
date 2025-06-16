c68c68312f53239072274641c95aa40320ed96c7
psh <todo220@naver.com>
2025-06-16 22:57:53 +0900

    Feat: DuplicateMealException.java - 중복 식단 등록 예외 처리

---
+++ src/main/java/com/myfitmate/myfitmate/exception/DuplicateMealException.java
@@ -0,0 +1,7 @@
+package com.myfitmate.myfitmate.exception;
+
+public class DuplicateMealException extends RuntimeException {
+    public DuplicateMealException(String message) {
+        super(message);
+    }
+}
