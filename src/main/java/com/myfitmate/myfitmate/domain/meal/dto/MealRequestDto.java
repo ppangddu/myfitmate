75b049c35e8ff322c8ab3735b47c18f8b68ccac3
psh <todo220@naver.com>
2025-06-16 22:52:05 +0900

    Feat: MealRequestDto.java - 식단 등록 요청 DTO 정의

---
+++ src/main/java/com/myfitmate/myfitmate/domain/meal/dto/MealRequestDto.java
@@ -0,0 +1,31 @@
+package com.myfitmate.myfitmate.domain.meal.dto;
+
+import com.myfitmate.myfitmate.domain.meal.entity.MealType;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+public class MealRequestDto {
+
+    @NotNull
+    private LocalDateTime eatTime;
+
+    @NotNull
+    private MealType mealType;
+
+    @NotNull
+    private List<FoodItem> foodList;
+
+    private boolean hasImage;
+
+    public record FoodItem(
+            @NotNull
+            Long foodId,
+            float quantity
+    ){}
+}
