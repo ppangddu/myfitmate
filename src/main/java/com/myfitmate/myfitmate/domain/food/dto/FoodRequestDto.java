4fda7628a3b7d9d6cfa7ed9df92b13625dffc2c0
psh <todo220@naver.com>
2025-06-16 22:50:15 +0900

    Feat: FoodRequestDto.java - 음식 등록 요청 DTO 정의

---
+++ src/main/java/com/myfitmate/myfitmate/domain/food/dto/FoodRequestDto.java
@@ -0,0 +1,15 @@
+package com.myfitmate.myfitmate.domain.food.dto;
+
+import lombok.Getter;
+
+@Getter
+public class FoodRequestDto {
+    private String name;
+    private Float standardAmount;
+    private Float calories;
+    private Float protein;
+    private Float fat;
+    private Float carbohydrate;
+    private Float sodium;
+
+}
